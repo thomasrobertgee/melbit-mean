4493a938ab63ce9f73b71c16d75e0d4f9fa79b68
Thomas Gee <thomasrobertgee@gmail.com>
2016-12-19 17:21:33 +1100

    fixed css issue, added controllers, fixed routes issues, moving on to HTML layouts

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-var homepageController = function (req, res) {
-  res.render('index', { title: 'Express' });
-};
+/* Location pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', homepageController);
+/* Other pages. */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
