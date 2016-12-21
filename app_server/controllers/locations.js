31396482673fd4d198719f42584174a250694cd0
Thomas Gee <thomasrobertgee@gmail.com>
2016-12-21 11:27:55 +1100

    added layout for Location-Info.jade file

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ module.exports.homelist = function(req, res){
 
 /* GET 'info' page */
 module.exports.locationInfo = function(req, res){
-  res.render('index', { title: 'Location info' });
+  res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
