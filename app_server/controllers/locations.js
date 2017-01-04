79b5756a9ef39015a56a428af75cb60a03af174c
Thomas Gee <thomasrobertgee@gmail.com>
2017-01-04 14:23:01 +1100

    changed the controllers so they generate the content for the views rather than having static, unchanging text in the views.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -23,17 +23,59 @@ module.exports.homelist = function(req, res){
       address: 'Brown Alley 564, Melbourne 3001',
       rating: 4,
       facilities: ['Pizza', 'No Mas', 'Premium Wifi'],
-      distance: '950m'
+      distance: '9500m'
     }]
   });
 };
 
 /* GET 'info' page */
 module.exports.locationInfo = function(req, res){
-  res.render('location-info', { title: 'Location info' });
+  res.render('location-info', {
+    title: 'Higher Ground',
+    pageHeader: {title: 'Higher Ground'},
+    sidebar: {
+      context: 'is somewhere to buy coffee with      Bitcoin.',
+      callToAction: 'If you\'ve been and you like it, or if you don\'t, please leave a review regardless.'
+    },
+    location: {
+      name: 'Higher Ground',
+      address: '123 Little Bourke Street, Melbourne 3000',
+      rating: 5,
+      facilities: ['Hot Drinks', 'Beer', 'Premium Wifi'],
+      coords: {lat: 51.455041, lng: -0.9690884},
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7.00am',
+        closing: '7.00pm',
+        closed: false
+      },{
+        days: 'Saturday',
+        opening: '8.00am',
+        closing: '6.00pm',
+        closed: false
+      },{
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Tommy Gee',
+        rating: 5,
+        timestamp: '16 February 2014',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+      },{
+        author: 'Baxter Callan',
+        rating: 4,
+        timestamp: '30 June 2014',
+        reviewText: 'Not bad hombre.'
+      }]
+    }
+  });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-  res.render('location-review-form', { title: 'Add review' });
+  res.render('location-review-form', {
+    title: 'Review Higher Ground on Melbit',
+    pageHeader: { title: 'Review Higher Ground'}
+  });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET home page. */
 module.exports.about = function(req, res) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', {
+    title: 'About Melbit',
+    content: 'Melbit was created to help people find businesses in Melbourne that accept Bitcoin. <br/><br/> The saints food bloggers, moomba the bulldogs the australian open carlton tullamarine, bill clinton ate two bowls hu tong dumplings the espy swanston footscray hobos, ticket inspector bespectacled girls north of the river MSAC secret laneway bars, formula one grand prix cold drip coffee old melbourne gaol.' 
+  });
 };
