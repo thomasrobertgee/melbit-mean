dc8f2c5d3995a404e2ea8fbed1f2832417864774
Thomas Gee <thomasrobertgee@gmail.com>
2017-01-19 16:27:35 +1100

    just committing because I've done quite a lot of code and I don't want to lost it. mostly just pushed ahead with the authentication, have created register and login forms, now modifying navigagtion as per login status

---
+++ app_client/common/services/authentication.service.js
@@ -0,0 +1,63 @@
+(function () {
+
+  angular
+    .module('melbitApp')
+    .service('authentication', authentication);
+
+  authentication.$inject = ['$window'];
+  function authentication ($window) {
+
+    var saveToken = function (token) {
+      $window.localStorage['melbit-token'] = token;
+    };
+
+    var getToken = function () {
+      return $window.localStorage['melbit-token'];
+    };
+
+    register = function(user) {
+      return $http.post('/api/register', user).success(function(data){
+        saveToken(data.token);
+      });
+    };
+
+    login = function(user) {
+      return $http.post('/api/login', user).success(function(data) {
+        saveToken(data.token);
+      });
+    };
+
+    logout = function() {
+      $window.localStorage.removeItem('melbit-token');
+    };
+
+    var isLoggedIn = function() {
+      var token = getToken();
+
+      if(token){
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    var currentUser = function() {
+      if(isLoggedIn()) {
+        var token = getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return {
+          email : payload.email,
+          name : payload.name
+        };
+      }
+    };
+
+    return {
+      saveToken : saveToken,
+      getToken : getToken
+    };
+  }
+
+}) ();
